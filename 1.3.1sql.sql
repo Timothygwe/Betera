c82f0228405fc2010de9979f7ddbf0225c0ab034
Timothygwe <144064459+Timothygwe@users.noreply.github.com>
2025-02-19 14:49:52 +0300

    Update 1.3.1sql.sql

--- 1.3.1sql.sql
+++ 1.3.1sql.sql
@@ -1,12 +1,12 @@
-
-SELECT DISTINCT player_id
-FROM bets b
-INNER JOIN events e on b.event_id = e.event_id
+SELECT *
+FROM bets
+JOIN  ON bets.event_id = events.event_id
 WHERE
-    event_stage = 'Prematch' AND
-    sport = 'E-Sports' AND
-    bet_size >= 10 AND
-    accepted_odd >= 1.5 AND
-    settlement_time <= '2022-03-15 12:00' AND
-    bet_type != 'System' AND
-    item_result NOT IN ('Return', 'Cashout', 'FreeBet');
+    create_time >= '2022-03-14 12:00:00'
+    AND event_stage = 'Prematch'
+    AND sport = 'E-Sports'
+    AND bet_size >= 10
+    AND accepted_odd >= 1.5
+    AND settlement_time <= '2022-03-15 12:00:00'
+    AND bet_type <> 'System'
+    AND item_result NOT IN ('Return', 'Cashout', 'FreeBet');
